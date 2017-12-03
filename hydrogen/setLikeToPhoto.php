9441bbdaecfad22fcf273abc261af8e44f4acef0
Jaime Escoto <jaime.escoto@hotmail.com>
2017-12-03 15:05:28 -0600

    Deleting unnecessary...

--- hydrogen/setLikeToPhoto.php
+++ hydrogen/setLikeToPhoto.php
@@ -4,54 +4,47 @@ include "connect.php";
 
 if (isset($_GET['q'])) {
     $q = ($_GET['q']);
-}
-else {
-    $q="";
+} else {
+    $q = "";
 }
 if (isset($_GET['ip'])) {
     $ip = ($_GET['ip']);
-}
-else {
-    $ip="";
+} else {
+    $ip = "";
 }
 
 if (isset($_GET['city'])) {
     $city = ($_GET['city']);
-}
-else {
-    $city="";
+} else {
+    $city = "";
 }
 if (isset($_GET['country_code'])) {
     $country_code = ($_GET['country_code']);
-}
-else {
-    $country_code="";
+} else {
+    $country_code = "";
 }
 if (isset($_GET['longitude'])) {
     $longitude = ($_GET['longitude']);
-}
-else {
-    $longitude=1;
+} else {
+    $longitude = 1;
 }
 if (isset($_GET['latitude'])) {
     $latutide = ($_GET['latitude']);
-}
-else {
-    $latutide=1;
+} else {
+    $latutide = 1;
 }
 
-$conn=getTheGalleryConnection();
+$conn = getTheGalleryConnection();
 if (!$conn) {
     die('Could not connect: ' . mysqli_error($conn));
 }
 $nowFormat = date('Y-m-d H:i:s');
 
-$sql="INSERT INTO Likes(photoLink, date, ip, country_code, city, latitude, longitude) VALUES ('".$q."','".$nowFormat."','".$ip."','".$country_code."','".$city."',".$latutide.",".$longitude.")";
+$sql = "INSERT INTO Likes(photoLink, date, ip, country_code, city, latitude, longitude) VALUES ('" . $q . "','" . $nowFormat . "','" . $ip . "','" . $country_code . "','" . $city . "'," . $latutide . "," . $longitude . ")";
 if ($result = $conn->query($sql)) {
 
-}
-else {
-  die('Could not connect: ' . mysqli_error($conn));
+} else {
+    die('Could not connect: ' . mysqli_error($conn));
 }
 mysqli_close($conn);
 
